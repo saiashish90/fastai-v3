790e35c54d8fb6c201edc6bbff2285e24ebf8713
saiashish90 <saiashish90@gmail.com>
2019-10-12 10:51:43 +0530

    dog breed app

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1c3YBTjTj6xl7QkcLwC49x_TBk5ixEhJ1?alt=media&key=AIzaSyBVLxQW4BS6qTdqRmdCvD6HQyj06No01lo'
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
